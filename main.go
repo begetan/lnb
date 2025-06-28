cdeab842a13357fae34e700e372108e3582bf442
Boris Nagaev <bnagaev@gmail.com>
2025-06-28 16:42:50 -0300

    use lndclient
    Use library lndclient instead of sub-module "client".
    Also updated dependencies.
    
    Use types like btcutil.Amount and lnwire.MilliSatoshi instead of ints to provide
    type safety.

--- main.go
+++ main.go
@@ -1,16 +1,20 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"os"
+	"os/user"
+	"path/filepath"
+	"strings"
 
+	"github.com/lightninglabs/lndclient"
+	"github.com/lightningnetwork/lnd/lnrpc/verrpc"
 	"github.com/urfave/cli/v2"
-
-	"google.golang.org/grpc"
 )
 
 const (
-	defaultLndDir			= "~/.lnd"
+	defaultLndDir           = "~/.lnd"
 	defaultDataDir          = "data"
 	defaultChainSubDir      = "chain"
 	defaultTLSCertFilename  = "tls.cert"
@@ -20,10 +24,85 @@ const (
 )
 
 var (
-	// maxMsgRecvSize is the largest message our client will receive. We
-	// set this to 200MiB atm.
-	maxMsgRecvSize = grpc.MaxCallRecvMsgSize(1 * 1024 * 1024 * 200)
+	// minRequiredLndVersion is the minimum required version of lnd that
+	// is compatible with the current version of the lnb.
+	minRequiredLndVersion = &verrpc.Version{
+		AppMajor: 0,
+		AppMinor: 17,
+		AppPatch: 0,
+	}
 )
+
+// cleanAndExpandPath expands environment variables and leading ~ in the
+// passed path, cleans the result, and returns it.
+// This function is taken from https://github.com/btcsuite/btcd
+func cleanAndExpandPath(path string) string {
+	if path == "" {
+		return ""
+	}
+
+	// Expand initial ~ to OS specific home directory.
+	if strings.HasPrefix(path, "~") {
+		var homeDir string
+		user, err := user.Current()
+		if err == nil {
+			homeDir = user.HomeDir
+		} else {
+			homeDir = os.Getenv("HOME")
+		}
+
+		path = strings.Replace(path, "~", homeDir, 1)
+	}
+
+	// NOTE: The os.ExpandEnv doesn't work with Windows-style %VARIABLE%,
+	// but the variables can still be expanded via POSIX-style $VARIABLE.
+	return filepath.Clean(os.ExpandEnv(path))
+}
+
+func getClient(callerCtx context.Context, ctx *cli.Context) (*lndclient.GrpcLndServices, error) {
+	// We'll now fetch the lnddir so we can make a decision  on how to
+	// properly read the macaroons (if needed) and also the cert. This will
+	// either be the default, or will have been overwritten by the end
+	// user.
+	lndDir := cleanAndExpandPath(ctx.String("lnddir"))
+
+	network := strings.ToLower(ctx.String("network"))
+
+	// If the macaroon path as been manually provided, then we'll only
+	// target the specified file.
+	var macPath string
+	if ctx.String("macaroonpath") != "" {
+		macPath = cleanAndExpandPath(ctx.String("macaroonpath"))
+	} else {
+		// Otherwise, we'll go into the path:
+		// lnddir/data/chain/<chain>/<network> in order to fetch the
+		// macaroon that we need.
+		macPath = filepath.Join(
+			lndDir, defaultDataDir, defaultChainSubDir, "bitcoin",
+			network, defaultMacaroonFilename,
+		)
+	}
+
+	tlsCertPath := cleanAndExpandPath(ctx.String("tlscertpath"))
+
+	// If a custom lnd directory was set, we'll also check if custom paths
+	// for the TLS cert and macaroon file were set as well. If not, we'll
+	// override their paths so they can be found within the custom lnd
+	// directory set. This allows us to set a custom lnd directory, along
+	// with custom paths to the TLS cert and macaroon file.
+	if lndDir != cleanAndExpandPath(defaultLndDir) {
+		tlsCertPath = filepath.Join(lndDir, defaultTLSCertFilename)
+	}
+
+	return lndclient.NewLndServices(&lndclient.LndServicesConfig{
+		LndAddress:         ctx.String("rpcserver"),
+		Network:            lndclient.Network(ctx.String("network")),
+		CustomMacaroonPath: macPath,
+		TLSPath:            tlsCertPath,
+		CheckVersion:       minRequiredLndVersion,
+		CallerCtx:          callerCtx,
+	})
+}
 
 func main() {
 	app := cli.NewApp()
